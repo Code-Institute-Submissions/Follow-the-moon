91207841ad9b59772c6415bf868e2e059b695c87
Daph1986 <daphne.heimgartner@gmail.com>
2021-02-06 12:41:40 +0100

    Made separate js file for function

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/* ------------- Redirect script ------------- */
+
+function redirectToDailyLife() {
+  location.replace("dailylife.html")
+}
+
+
+
+/* ------------- Confirmation script ------------- */
+
+function confirmation() {
+  alert("Thank you for signing up!");
+}
+
+
+
